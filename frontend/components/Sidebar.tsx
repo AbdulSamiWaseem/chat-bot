ce690ee9f223b8f2e9767a9c7d54ba4c98e72add
Abdul Sami Waseem <abdulsamiwaseem26@gmail.com>
2026-07-01 18:08:34 +0500

    style: update sidebar layout styling

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -8,17 +8,17 @@ export default function Sidebar() {
     await signOut();
   };
   return (
-    <Box sx={{ width: 250, bgcolor: "black", color: "white", p: 2, height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "start" }}>
-      <Typography variant="h6">Chat Bot</Typography>
+    <Box sx={{ width: 250, bgcolor: "black", color: "white", height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "start" }}>
+      <Typography variant="h6" sx={{ borderBottom: "1px solid white", width: "100%", padding: "16px 0px 16px 16px" }}>Chat Bot</Typography>
       <Button
         onClick={handleLogout}
         variant="contained"
         color="error"
         size="small"
-        sx={{ textTransform: "none" }}
+        sx={{ textTransform: "none", m: 2 }}
       >
         Logout
       </Button>
-    </Box>
+    </Box >
   );
 }
