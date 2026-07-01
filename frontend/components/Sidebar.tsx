022e932ca82bc290a99ded02dd7b47edfcb6ed38
Abdul Sami Waseem <abdulsamiwaseem26@gmail.com>
2026-07-01 17:56:35 +0500

    feat: add logout

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,9 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { useAuth } from "@/app/services/useAuth";
 
 export default function Sidebar() {
+  const { signOut } = useAuth();
+
+  const handleLogout = async () => {
+    await signOut();
+  };
   return (
-    <Box sx={{ width: 250, bgcolor: "black", color: "white", p: 2, height: "100%" }}>
+    <Box sx={{ width: 250, bgcolor: "black", color: "white", p: 2, height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "start" }}>
       <Typography variant="h6">Chat Bot</Typography>
+      <Button
+        onClick={handleLogout}
+        variant="contained"
+        color="error"
+        size="small"
+        sx={{ textTransform: "none" }}
+      >
+        Logout
+      </Button>
     </Box>
   );
 }
